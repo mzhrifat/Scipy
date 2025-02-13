957547a89e3edbf0af98dcd77e0e0cb0361a696c
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-02-14 01:19:46 +0600

    all value commit

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 #Metric (SI) Prefixes:
 
-
+"""
 from scipy import constants
 
 print(constants.yotta)    #1e+24
@@ -22,3 +22,109 @@ print(constants.pico)     #1e-12
 print(constants.femto)    #1e-15
 print(constants.atto)     #1e-18
 print(constants.zepto)    #1e-21
+"""
+
+#Binary Prefixes
+
+from scipy import constants
+
+print(constants.kibi)    #1024
+print(constants.mebi)    #1048576
+print(constants.gibi)    #1073741824
+print(constants.tebi)    #1099511627776
+print(constants.pebi)    #1125899906842624
+print(constants.exbi)    #1152921504606846976
+print(constants.zebi)    #1180591620717411303424
+print(constants.yobi)    #1208925819614629174706176
+
+#Mass
+
+print(constants.gram)        #0.001
+print(constants.metric_ton)  #1000.0
+print(constants.grain)       #6.479891e-05
+print(constants.lb)          #0.45359236999999997
+print(constants.pound)       #0.45359236999999997
+print(constants.oz)          #0.028349523124999998
+print(constants.ounce)       #0.028349523124999998
+print(constants.stone)       #6.3502931799999995
+print(constants.long_ton)    #1016.0469088
+print(constants.short_ton)   #907.1847399999999
+print(constants.troy_ounce)  #0.031103476799999998
+print(constants.troy_pound)  #0.37324172159999996
+print(constants.carat)       #0.0002
+print(constants.atomic_mass) #1.66053904e-27
+print(constants.m_u)         #1.66053904e-27
+print(constants.u)           #1.66053904e-27
+
+#Angle
+print(constants.degree)     #0.017453292519943295
+print(constants.arcmin)     #0.0002908882086657216
+print(constants.arcminute)  #0.0002908882086657216
+print(constants.arcsec)     #4.84813681109536e-06
+print(constants.arcsecond)  #4.84813681109536e-06
+
+#Time
+print(constants.minute)      #60.0
+print(constants.hour)        #3600.0
+print(constants.day)         #86400.0
+print(constants.week)        #604800.0
+print(constants.year)        #31536000.0
+print(constants.Julian_year) #31557600.0
+
+#Length:
+
+print(constants.inch)              #0.0254
+print(constants.foot)              #0.30479999999999996
+print(constants.yard)              #0.9143999999999999
+print(constants.mile)              #1609.3439999999998
+print(constants.mil)               #2.5399999999999997e-05
+print(constants.pt)                #0.00035277777777777776
+print(constants.point)             #0.00035277777777777776
+print(constants.survey_foot)       #0.3048006096012192
+print(constants.survey_mile)       #1609.3472186944373
+print(constants.nautical_mile)     #1852.0
+print(constants.fermi)             #1e-15
+print(constants.angstrom)          #1e-10
+print(constants.micron)            #1e-06
+print(constants.au)                #149597870691.0
+print(constants.astronomical_unit) #149597870691.0
+print(constants.light_year)        #9460730472580800.0
+print(constants.parsec)            #3.0856775813057292e+16
+
+#Pressure
+print(constants.atm)         #101325.0
+print(constants.atmosphere)  #101325.0
+print(constants.bar)         #100000.0
+print(constants.torr)        #133.32236842105263
+print(constants.mmHg)        #133.32236842105263
+print(constants.psi)         #6894.757293168361
+
+#Area
+print(constants.hectare) #10000.0
+print(constants.acre)    #4046.8564223999992
+
+#Energy
+print(constants.eV)            #1.6021766208e-19
+print(constants.electron_volt) #1.6021766208e-19
+print(constants.calorie)       #4.184
+print(constants.calorie_th)    #4.184
+print(constants.calorie_IT)    #4.1868
+print(constants.erg)           #1e-07
+print(constants.Btu)           #1055.05585262
+print(constants.Btu_IT)        #1055.05585262
+print(constants.Btu_th)        #1054.3502644888888
+print(constants.ton_TNT)       #4184000000.0
+
+#Power
+
+print(constants.hp)         #745.6998715822701
+print(constants.horsepower) #745.6998715822701
+
+#force
+
+print(constants.dyn)             #1e-05
+print(constants.dyne)            #1e-05
+print(constants.lbf)             #4.4482216152605
+print(constants.pound_force)     #4.4482216152605
+print(constants.kgf)             #9.80665
+print(constants.kilogram_force)  #9.80665
