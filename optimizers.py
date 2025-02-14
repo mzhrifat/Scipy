2d9acf88c53e8d9f9a4c0994c5aba85f1fe93699
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-02-15 02:09:57 +0600

    minimize commit

--- optimizers.py
+++ optimizers.py
@@ -1,3 +1,5 @@
+
+"""
 from scipy.optimize import root
 from math import cos
 
@@ -8,5 +10,15 @@ myroot = root(eqn,0)
 
 print(myroot.x)
 print (myroot)
+"""
+#Minimizing a Function
 
+from scipy.optimize import minimize
+
+def eqn(x):
+    return x ** 3 + x + 2
+
+mymin=minimize(eqn,0,method='BFGS')
+
+print(mymin)
 
