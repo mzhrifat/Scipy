eef1b825a0f923228716fb3a75e78a40d2c969d3
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-02-18 01:41:59 +0600

    Triangulation commit

---
+++ Spatial_data.py
@@ -0,0 +1,30 @@
+"""
+from scipy.spatial import distance
+
+point1=(1,2)
+point2=(4,6)
+
+dist = distance.euclidean(point1, point2)
+
+print("Euclidean Distance:",dist)
+"""
+#Triangulation
+
+import numpy as np
+from scipy.spatial import Delaunay, convex_hull_plot_2d
+import matplotlib.pyplot as plt
+
+points=np.array([
+    [2,4],
+    [3,4],
+    [3,0],
+    [2,2],
+    [4,1]
+])
+
+simplices=Delaunay(points).simplices
+
+plt.triplot(points[:,0],points[:,1],simplices)
+plt.scatter(points[:,0],points[:,1],color='r')
+plt.savefig("Triangulation.png",format='png')
+#plt.show()
