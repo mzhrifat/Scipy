8cb8058a1178fd8985196a39bbf5be32a572eec0
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-02-17 23:52:28 +0600

    dijkstra

--- Graphs.py
+++ Graphs.py
@@ -1,3 +1,4 @@
+"""
 import numpy as np
 from scipy.sparse.csgraph import connected_components
 from scipy.sparse import csr_matrix
@@ -11,3 +12,20 @@ arr = np.array([
 newarr = csr_matrix(arr)
 
 print(connected_components(newarr))
+
+"""
+#Dijkstra
+
+import numpy as np
+from scipy.sparse.csgraph import dijkstra
+from scipy.sparse import csr_matrix
+
+arr = np.array([
+  [0, 1, 2],
+  [1, 0, 0],
+  [2, 0, 0]
+])
+
+newarr = csr_matrix(arr)
+
+print(dijkstra(newarr, return_predecessors=True, indices=0))
