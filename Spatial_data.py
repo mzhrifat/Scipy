8e97abe01e886ff5291954023eb28b19fd2d84fd
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-02-19 00:52:41 +0600

    covexhull commit

--- Spatial_data.py
+++ Spatial_data.py
@@ -35,7 +35,7 @@ plt.grid(True)
 
 plt.savefig("Triangulation.png",format='png')
 #plt.show()
-"""
+
 #3d
 import numpy as np
 from scipy.spatial import Delaunay
@@ -69,9 +69,36 @@ for simplex in simplices:
 # গ্রাফ দেখানো
 plt.show()
 
-
-
+"""
 
 
 #Convex Hull
 
+import numpy as np
+
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+
+points = np.array([
+  [2, 4],
+  [3, 4],
+  [3, 0],
+  [2, 2],
+  [4, 1],
+  [1, 2],
+  [5, 0],
+  [3, 1],
+  [1, 2],
+  [0, 2]
+])
+
+hull=ConvexHull(points)
+
+hull_points=hull.simplices
+
+plt.scatter(points[:,0],points[:,1])
+for simplex in hull_points:
+    plt.plot(points[simplex,0],points[simplex,1],'k-',color='r')
+
+plt.show()
+#plt.savefig("convex.png",format='png')
