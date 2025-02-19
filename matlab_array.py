3f9b76b192b0becb23d80353b705f886f74d4fe5
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-02-20 01:36:08 +0600

    h5py commit

--- matlab_array.py
+++ matlab_array.py
@@ -5,7 +5,7 @@ import numpy as np
 arr=np.arange(10)
 
 io.savemat('arr.mat',{"vec":arr})
-"""
+
 
 from scipy import io
 import numpy as np
@@ -20,3 +20,17 @@ mydata=io.loadmat('arr.mat',squeeze_me=True)
 
 #print(mydata)
 print(mydata['vec'])
+"""
+
+import h5py
+
+# HDF5 MATLAB ফাইল ওপেন করা
+file = h5py.File('example_v7_3.mat', 'r')
+
+# ফাইলের কী গুলো দেখা
+print(list(file.keys()))
+
+# কোনো নির্দিষ্ট dataset লোড করা
+dataset = file['my_matrix'][:]
+print(dataset)
+
