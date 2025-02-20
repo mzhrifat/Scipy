7f8b077ec8e3695cf69dd8db8e18fc1d53a3e84b
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-02-21 01:40:23 +0600

    normaltest commit

--- Statistical Significance Tests .py
+++ Statistical Significance Tests .py
@@ -76,7 +76,7 @@ v=np.random.normal(size=100)
 res=describe(v)
 
 print(res)
-"""
+
 #Kurtosis:
 
 import numpy as np
@@ -85,4 +85,13 @@ from scipy.stats import skew, kurtosis
 v=np.random.normal(size=100)
 print(skew(v))
 print(kurtosis(v))
+"""
+#normaltest
+
+import numpy as np
+from scipy.stats import normaltest
+
+v=np.random.normal(size=100)
+
+print(normaltest(v))
 
